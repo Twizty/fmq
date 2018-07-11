542b6cf3b3f6647150a6d9352e21b1c397d488c1
Maxim Davydov <maximdavydoff@yandex.ru>
2018-07-11 18:21:51 +0300

    Added ability to delete sockets from storage if connection is closed without exiting

--- src/main/scala/EventSink.scala
+++ src/main/scala/EventSink.scala
@@ -20,6 +20,7 @@ class EventSink[F[_]](state: State)(implicit F: Effect[F], ec: ExecutionContext)
           .evalMap[Unit](handleEvent(_, s, sig))
           .attempt
           .interruptWhen(sig)
+          .onFinalize(F.delay { state.deleteHost(s.addr) })
       }
     )
 
