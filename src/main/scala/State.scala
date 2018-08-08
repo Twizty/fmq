b0a766283eac0b1d63fa3366d4eaad55c86ad366
Maxim Davydov <maximdavydoff@yandex.ru>
2018-08-08 09:43:31 +0300

    Rewrote State with (almost) unblocking ConcurrentHashMap

--- src/main/scala/State.scala
+++ src/main/scala/State.scala
@@ -6,91 +6,73 @@ sealed trait SubscriptionState
 case object Subscribed extends SubscriptionState
 case object Unsubscribed extends SubscriptionState
 
-class State {
-  import collection.mutable
+class State extends collection.convert.AsScalaConverters {
+  import collection.concurrent
+  import java.util.concurrent.ConcurrentHashMap
 
-  private val state = mutable.Map[String, Map[String, SubscriptionState]]()
-  private val rwlock = new ReentrantReadWriteLock()
+  private val state: concurrent.Map[String, concurrent.Map[String, SubscriptionState]] =
+    mapAsScalaConcurrentMap(new ConcurrentHashMap[String, concurrent.Map[String, SubscriptionState]]())
 
-  def exchangeExists(exchange: String): Boolean = {
-    withLock(rwlock.readLock()) {
+  def newMap(k: String, v: SubscriptionState) = {
+    val m = mapAsScalaConcurrentMap(new ConcurrentHashMap[String, SubscriptionState]())
+    m.put(k, v)
+    m
+  }
+
+  def exchangeExists(exchange: String): Boolean =
+    state.get(exchange) match {
+      case Some(map) => map.exists { case (_, s) => s == Subscribed }
+      case None => false
+    }
+
+  def hostState(exchange: String, host: String): Option[SubscriptionState] =
+    state.get(exchange) match {
+      case Some(map) => map.get(host)
+      case None => None
+    }
+
+  def add(host: String, exchanges: Array[String]): Unit =
+    exchanges.foreach { exchange =>
       state.get(exchange) match {
-        case Some(map) => map.exists { case (_, s) => s == Subscribed }
-        case None => false
+        case Some(s) => s.put(host, Subscribed)
+        case None =>
+          state.putIfAbsent(exchange, newMap(host, Subscribed)) match {
+            case Some(old) => old.put(host, Subscribed)
+            case None => ()
+          }
       }
     }
-  }
 
-  def hostState(exchange: String, host: String): Option[SubscriptionState] = {
-    withLock(rwlock.readLock()) {
+  def deleteExchanges(host: String, exchanges: Array[String]): Unit =
+    exchanges.foreach { exchange =>
       state.get(exchange) match {
-        case Some(map) => map.get(host)
-        case None => None
+        case Some(s) => s.remove(host)
+        case None => ()
       }
     }
-  }
 
-  def add(host: String, exchanges: Array[String]): Unit = {
-    withLock(rwlock.writeLock()) {
-      exchanges.foreach { exchange =>
-        state.get(exchange) match {
-          case Some(s) => state.update(exchange, s + (host -> Subscribed))
-          case None => state(exchange) = Map(host -> Subscribed)
-        }
+  def unsubscribeExchanges(host: String, exchanges: Array[String]): Unit =
+    exchanges.foreach { exchange =>
+      state.get(exchange) match {
+        case Some(s) => s.update(host, Unsubscribed)
+        case None => ()
       }
     }
-  }
 
-  def deleteExchanges(host: String, exchanges: Array[String]): Unit = {
-    withLock(rwlock.writeLock()) {
-      exchanges.foreach { exchange =>
-        state.get(exchange) match {
-          case Some(s) => state.update(exchange, s - host)
-          case None => ()
-        }
+  def unsubscribeHost(host: String): Unit =
+    state.keys.foreach { exchange =>
+      state.get(exchange) match {
+        case Some(s) => s.update(host, Unsubscribed)
+        case None => ()
       }
     }
-  }
 
-  def unsubscribeExchanges(host: String, exchanges: Array[String]): Unit = {
-    withLock(rwlock.writeLock()) {
-      exchanges.foreach { exchange =>
-        state.get(exchange) match {
-          case Some(s) => state.update(exchange, s.updated(host, Unsubscribed))
-          case None => ()
-        }
+  def deleteHost(host: String): Unit =
+    state.keys.foreach { exchange =>
+      state.get(exchange) match {
+        case Some(s) => s.remove(host)
+        case None => ()
       }
     }
-  }
-
-  def unsubscribeHost(host: String): Unit = {
-    withLock(rwlock.writeLock()) {
-      state.keys.foreach { exchange =>
-        state.get(exchange) match {
-          case Some(s) => state.update(exchange, s.updated(host, Unsubscribed))
-          case None => ()
-        }
-      }
-    }
-  }
-
-  def deleteHost(host: String): Unit = {
-    withLock(rwlock.writeLock()) {
-      state.keys.foreach { exchange =>
-        state.get(exchange) match {
-          case Some(s) => state.update(exchange, s - host)
-          case None => ()
-        }
-      }
-    }
-  }
-
-  def withLock[A](lock: Lock)(action: => A): A = {
-    lock.lock()
-    try {
-      action
-    } finally {
-      lock.unlock()
-    }
-  }
 }
+
